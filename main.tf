8781a8a8b8ab9ace2ee7b400778b47d936a69802
tnakajo <tnakajo@au1.ibm.com>
2020-07-05 15:34:51 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "ibm_container_cluster" "testacc_cluster" {
   name            = "mycluster0705"
   datacenter      = "dal10"
   machine_type    = "b3c.4x16"
-  #hardware        = "shared"
+  hardware        = "shared"
   #public_vlan_id  = "123456"
   #private_vlan_id = "654321"
   #subnet_id       = ["1154643"]
