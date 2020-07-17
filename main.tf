c1d66f52e8181b685d9645c7c2e5025008ec9d69
tnakajo <tnakajo@au1.ibm.com>
2020-07-17 13:14:12 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 resource "ibm_container_cluster" "testacc_cluster" {
   name            = "mycluster0717"
   datacenter      = "wdc04"
-  #machine_type    = "free"
-  #machine_type    = "b2c.4x16"
+  machine_type    = "b2c.4x16"
   hardware        = "shared"
   #public_vlan_id  = "123456"
   #private_vlan_id = "654321"
