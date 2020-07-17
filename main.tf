40b9290e0b55b52a6e1dbbe97345414d4873571b
tnakajo <tnakajo@au1.ibm.com>
2020-07-17 11:20:41 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,10 +9,10 @@ resource "ibm_container_cluster" "testacc_cluster" {
   #default_pool_size      = 1
   
   tags             = [
-          + "Shared         : No",
+          "Shared         : No",
           + "TechnicalOwner : tnakajo",
           + "environment    : kp-cdo-roks-eng-02",
           + "kaiserpermanente-provisioner : terraform",
-          + "kaiserpermanente-provisioner-cluster-name  : kp-cdo-roks-eng-02",
+          + "kaiserpermanente-provisioner-cluster-name  : kp-cdo-roks-eng-02"
         ]
 }
