864e3ad65aad0017d6973e2e04ad93417ebd4ce8
tnakajo <tnakajo@au1.ibm.com>
2020-10-19 12:51:03 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-resource "ibm_container_cluster" "testacc_cluster" {
-  name            = "mycluster07171326"
-  datacenter      = "dal12"
-  machine_type    = "b2c.4x16"
-  hardware        = "shared"
-  public_vlan_id  = "956"
-  private_vlan_id = "1378"
-  #subnet_id       = ["1154643"]
-  #default_pool_size      = 1
-  
-  tags             = [
-          "TagA", "TagB", "TagC"
-        ]
+resource "ibm_iam_user_invite" "invite_user" {
+    users = ["test@in.ibm.com"]
 }
