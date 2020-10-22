e9698a1f21b99204adbc2558910a7e8912c1cf7e
hkantare <hkantare@in.ibm.com>
2020-10-22 12:26:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ data "ibm_iam_user_policy" "testacc_ds_user_policy" {
   ibm_id = "hkantare@in.ibm.com"
 }
 
-resource "ibm_iam_user_invite" "invite_user" {
+/*resource "ibm_iam_user_invite" "invite_user" {
     users = ["test@in.ibm.com"]
-}
+}*/
